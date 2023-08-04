65b5dcddbdd70e1eb26f34fa42d80a1199594f53
ighiba <ighiba18@gmail.com>
2023-08-04 21:54:03 +0300

    Added TextFieldDelegate

---
+++ SortingAlgorithms/Helpers/TextFieldDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  TextFieldDelegate.swift
+//  SortingAlgorithms
+//
+//  Created by Ivan Ghiba on 04.08.2023.
+//
+
+import Cocoa
+
+class TextFieldDelegate: NSObject, NSTextFieldDelegate {
+    let textFieldTextDidChangeHandler: ((NSTextField) -> Void)?
+    
+    init(textChangeHandler: ((NSTextField) -> Void)?) {
+        self.textFieldTextDidChangeHandler = textChangeHandler
+    }
+
+    func controlTextDidChange(_ obj: Notification) {
+        guard let textField = obj.object as? NSTextField else { return }
+        textFieldTextDidChangeHandler?(textField)
+    }
+}
